c72ceef2861f6a67517fbd877288a21cb306d42b
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-05-14 17:51:04 -0300

    feat: add prisma and config and movies list

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@
 // const teste = "fgfghgfgg"
 
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
 //GET, POST, PUT, PATCH, DELETE
 
-app.get("/movies", (req, res) => {
-    res.send("Listagem de filmes");
+app.get("/movies",  async  (req, res) => {
+    const movies = await prisma.movie.findMany();
+    res.json(movies);
 });
 
 app.listen(port, () => {
