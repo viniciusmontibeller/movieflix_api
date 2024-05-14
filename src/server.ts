b9c35b6f41f195df5c2ebc1a5023409359debd50
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-05-14 16:26:40 -0300

    feat: add express with initial config

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,17 @@
 // node não entende typescript, precisa-se baixar @types/node
-const teste = "fgfghgfgg";
+// const teste = "fgfghgfgg"
+
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+//GET, POST, PUT, PATCH, DELETE
+
+app.get("/movies", (req, res) => {
+    res.send("Listagem de filmes");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor em execução na porta ${port}`);
+});
