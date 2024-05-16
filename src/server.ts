5acf0607863c53fcfcbbc30b0d51e484842ae2ac
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-05-16 11:31:30 -0300

    feat: add movies update

--- src/server.ts
+++ src/server.ts
@@ -50,6 +50,39 @@ app.post("/movies", async (req, res) => {
     res.status(201).send();
 });
 
+app.put("/movies/:id", async (req, res) => {
+    //pegar o id do registro que vai seer atualizado
+    //pegar os dados do filme que sera atualizado e atualizar ele com o prisma
+    //retornar o status correto informando que o filme foi atualizado
+    console.log(req.params.id);
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id,
+            }
+        });
+
+        if (!movie) {
+            return res.send(404).send({ message: "Filme não encontrado" });
+        }
+
+        const data = { ...req.body };
+        data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+
+        await prisma.movie.update({
+            where: {
+                id,
+            },
+            data: data,
+        });
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao atualizar o resgistro do filme" });
+    }
+    res.status(200).send();
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
 });
